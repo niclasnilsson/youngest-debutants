a53e3fa26a328ef095bf8f35ea708f2b037b8817
Niclas Nilsson <niclas.nilsson@systemite.se>
2019-02-18 10:54:44 +0100

    Add initial Program.cs

--- YoungestDebutant/Program.cs
+++ YoungestDebutant/Program.cs
@@ -8,6 +8,27 @@ namespace YoungestDebutant
 {
     class Program
     {
+        //
+        //  Using only things from the standard library, create a 
+        //  program that prints out the youngest debutant or debutants 
+        //  in the form:
+        //
+        //  'Duane Allman was 21 when Hour Glass was released.'"
+        //
+
+        static string indata = @"
+            Duane Allman, 1946, Hour Glass, 1967
+            Jeff Beck, 1944, Five Live Yardbirds, 1964
+            Eric Clapton, 1945, Five Live Yardbirds, 1964
+            John Frusciante, 1970, Mother's Milk, 1989
+            Jimi Hendrix, 1942, Are You Experienced?, 1967
+            David Howell Evans(The Edge), 1961, Boy, 1980
+            Jimmy Page, 1944, Little Games, 1967
+            Steve Vai, 1960, Tinsel Town Rebellion, 1981
+            Eddie Van Halen, 1955, Van Halen, 1978
+            Tom Morello, 1964, Rage against the machine, 1992 
+            ";
+
         static void Main(string[] args)
         {
         }
